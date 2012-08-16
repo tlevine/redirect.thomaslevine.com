dd6e9ffdc2919519b99b17cf792c127d134af44d
Thomas Levine <occurrence@thomaslevine.com>
2012-08-16 04:47:45 -0400

    function names

--- application/app.py
+++ application/app.py
@@ -1,1 +1,19 @@
-'Application goes here.'
+'Application goes here.':
+
+def _validate_request_id('/srv/index.html'):
+
+def _validate_response_code(302):
+
+def _validate_response_code('303'):
+
+def _validate_('http://example.com;'):
+
+def _validate_('http://example.com{'):
+
+def _validate_('http://example.com}'):
+
+def _add_http('http://example.com/foo bar'):
+
+def _remove_http('example.com/foo bar'):
+
+def test_remove_http():
